91619f51d184c0938548dfda0339e88b0cecd98d
莫德芳 <20261647@qq.com>
2018-12-13 16:31:40 +0800

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { HelloComponent } from './hello/hello.component';
 import { HttpClientModule } from '@angular/common/http';
+import {AlertModule, ButtonsModule} from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,8 @@ import { HttpClientModule } from '@angular/common/http';
     HelloComponent
   ],
   imports: [
+    ButtonsModule.forRoot(),
+    AlertModule.forRoot(),
     BrowserModule,
     HttpClientModule
   ],
